44149565a925719405e23e47c26c71499d3c417f
Uizz Underweasel <uizz@outlook.com>
2018-04-18 23:19:24 +0300

    nuget update

--- MVPathway.Utils/Views/NavigationStackDebuggerView.xaml.cs
+++ MVPathway.Utils/Views/NavigationStackDebuggerView.xaml.cs
@@ -1,10 +1,8 @@
-﻿
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MVPathway.Utils.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NavigationStackDebuggerView : ContentView
     {
         public NavigationStackDebuggerView()
